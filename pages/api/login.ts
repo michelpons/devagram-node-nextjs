47f3e7abdc68985a8c2ac6635b1e3a7b0192bb1c
Michel Pons <michelpons.dev@gmail.com>
2022-10-07 17:11:04 -0300

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -13,5 +13,5 @@ export default (
         }        
         return res.status(400).json({erro : 'Usuario ou senha inválidos!'});
     } 
-    return res.status(405).json({erro : 'Metodo informado não é válido!'});     
+    return res.status(405).json({erro : 'Metodo informado não é válido!'});      
 }    
