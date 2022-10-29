d5f66d19c3ff9953544ae50a809938e72973030c
Michel Pons <michelpons.dev@gmail.com>
2022-10-28 23:02:21 -0300

    Upload de imagem para o cosmic usando o multer

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -4,47 +4,60 @@ import type { CadastroRequisicao } from '../../types/CadastroRequisicao';
 import {UsuarioModel} from '../../models/UsuarioModel';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
 import md5 from 'md5';
+import {upload, uploadImagemCosmic} from '../../services/uploadImagemCosmic';
+import nc from 'next-connect';
 
-
-const endPointCadastro = 
-    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
-
-    if(req.method === 'POST'){
+const handler = nc()
+    .use(upload.single('file'))
+    .post(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+        try{
+            console.log('cadastro endpoint', req.body);
         const usuario = req.body as CadastroRequisicao;
-
-        if(!usuario.nome || usuario.nome.length < 2){
-            return res.status(400).json({ erro : 'Nome invalido'});
-        }
-
-        if(!usuario.email || usuario.email.length < 5
-            || !usuario.email.includes('@')
-            || !usuario.email.includes('.')){
-            return res.status(400).json({ erro : 'Email invalido'});
+    
+            if(!usuario.nome || usuario.nome.length < 2){
+                return res.status(400).json({ erro : 'Nome invalido'});
             }
+    
+            if(!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({ erro : 'Email invalido'});
+                }
+            
+            if(!usuario.senha || usuario.senha.length < 4){
+                return res.status(400).json({ erro : 'Senha invalida'});
+            } 
+    
+            // validacao se ja existe usuario com o mesmo email
+            const usuariosComMesmoEmail = UsuarioModel.find({email : usuario.email});
+            if(usuariosComMesmoEmail && (await usuariosComMesmoEmail).length > 0){
+                return res.status(400).json({ erro : 'Usuario ja esxite no Banco de dados!'});
+             }
+
+            // enviar a imagem do multer para o cosmic
+            const image = await uploadImagemCosmic(req);
         
-        if(!usuario.senha || usuario.senha.length < 4){
-            return res.status(400).json({ erro : 'Senha invalida'});
-        } 
+            // salvar no banco de dados
+                const usuarioASerSalvo = {
+                    nome : usuario.nome,
+                    email : usuario.email,
+                    senha : md5(usuario.senha),
+                    avatar : image?.media?.url
+                }
+                await UsuarioModel.create(usuarioASerSalvo);
+                return res.status(200).json({msg : 'Usuarios criado com sucesso '});
+            
+        }catch(e){
+            console.log(e);
+            return res.status(500).json({msg : 'Erro ao cadastrar usuario '});
+        }
+});
 
-        // validacao se ja existe usuario com o mesmo email
-        const usuariosComMesmoEmail = UsuarioModel.find({email : usuario.email});
-        if(usuariosComMesmoEmail && (await usuariosComMesmoEmail).length > 0){
-            return res.status(400).json({ erro : 'Usuario ja esxite no Banco de dados!'});
-         }
-
-
-        // salvar no banco de dados
-            const usuarioASerSalvo = {
-                nome : usuario.nome,
-                email : usuario.email,
-                senha : md5(usuario.senha)
-            }
-            await UsuarioModel.create(usuarioASerSalvo);
-            return res.status(200).json({msg : 'Usuarios criado com sucesso '});
-
+export const config = {
+    api : {
+        bodyParser : false
     }
-    return res.status(405).json({erro : 'Metodo informado nao e valido'});
+    
 }
-
-export default conectarMongoDB(endPointCadastro);
-
+    
+export default conectarMongoDB(handler);
